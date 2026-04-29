0bc5cd855493133f4406674513c4a0fc70e8da1d
Nazmul Rahat <nazmulrahat786.13@gmail.com>
2026-04-30 03:57:35 +0600

    first commitdsss

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Bottols from './Bottols';
 const App = () => {
   return (
     <div>
+
+      <h1 className='text-center text-3xl font-extrabold my-1 '>BottleStore</h1>
       <Bottols></Bottols>
       
     </div>
